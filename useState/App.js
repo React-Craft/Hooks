f97b2f073d2b8d372dcc2849cef0ab5131e885dd
YOON-CC <whdbscks77@gmail.com>
2025-02-22 14:33:09 +0900

    feat: 재렌더링시, 상태 index 초기화

--- useState/App.js
+++ useState/App.js
@@ -1,22 +1,34 @@
 function App() {
+  resetStateIndex();
+  console.log("✅✅✅ 랜더링 ✅✅✅");
   const [a, setA] = useState(0);
+  const [b, setB] = useState(0);
+
   // const [b, setB] = useState("b");
 
   // console.log(a(), b());
   // setB("c");
   // console.log(a(), b());
 
-  function increase() {
+  function increaseA() {
     setA(a() + 1);
   }
 
+  function increaseB() {
+    setB(b() + 1);
+  }
+
   console.log(a());
+  console.log(b());
 
   document.getElementById("app").innerHTML = `
     <h1>count: ${a()}</h1>
     <button id="a">클릭</button>
+    <h1>count: ${b()}</h1>
+    <button id="b">클릭</button>
   `;
-  document.getElementById("a").addEventListener("click", increase);
+  document.getElementById("a").addEventListener("click", increaseA);
+  document.getElementById("b").addEventListener("click", increaseB);
 }
 
 App();
--- useState/useState.js
+++ useState/useState.js
@@ -1,14 +1,16 @@
+// 외부저장소 역할
 state = [];
 stateIndex = 0;
 
+// 이까지가 useState
 function useState(initialValue) {
   const currentIndex = stateIndex;
 
-  console.log(1, state[currentIndex]);
+  // console.log(1, state[currentIndex]);
   if (state[currentIndex] === undefined) {
-    console.log(2, state[currentIndex]);
+    // console.log(2, state[currentIndex]);
     state[currentIndex] = initialValue;
-    console.log("최초 init", state[currentIndex]);
+    // console.log("최초 init", state[currentIndex]);
   }
 
   function getState() {
@@ -16,16 +18,29 @@ function useState(initialValue) {
   }
 
   function setState(newValue) {
-    console.log("방금 들어옴", newValue);
+    // console.log("방금 들어옴", newValue);
 
     state[currentIndex] = newValue;
-    console.log("방금 바뀜", state);
-
-    return "값 추가";
+    console.log(
+      "현재 index",
+      currentIndex,
+      "현재 전체 state",
+      state,
+      "그리고 그 index에 대한 값",
+      state[currentIndex]
+    );
+    // console.log("방금 바뀜", state);
+    // 재렌더링
+    App();
   }
 
+  //useState 갯수만큼 중가함
   stateIndex++;
 
-  console.log(state);
+  console.log("useState호출시, 현재 상태", state);
   return [getState, setState];
+}
+
+function resetStateIndex() {
+  stateIndex = 0;
 }
