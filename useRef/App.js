0b3a8e65e769b95b0d805b7a4bbfecac29e31ef8
YOON-CC <whdbscks77@gmail.com>
2025-03-02 02:27:18 +0900

    feat: 돔객체 접근하는 것처럼 ref 임시 구현

--- useRef/App.js
+++ useRef/App.js
@@ -2,14 +2,15 @@ function App() {
   stateStore.resetStateIndex();
   console.log("✅✅✅ 랜더링 ✅✅✅");
 
-  const [getCount, setCount] = useState(0);
   const [a, setA] = useState("True");
   const [b, setB] = useState(0);
-  // const [obj, setObj] = useState({ value: 0 });
-  const objRef = useRef({ value: 0 }); // ✅ useRef로 변경
+
+  const objRef = useRef({ value: 0 });
+  const buttonRef = useRef(null); // 초기값은 null
+
   useEffect(() => {
     console.log("🎯🎯🎯🎯🎯🎯🎯🎯🎯🎯🎯🎯🎯🎯");
-  }, [objRef.current]); // ✅ useRef를 사용할 경우 의존성 배열에서 변경 감지 X
+  }, [objRef.current]);
 
   function handleUpdateText() {
     setA(a() === "True" ? "false" : "True");
@@ -18,33 +19,30 @@ function App() {
   function increase() {
     setB(b() + 1);
   }
-  console.log("dd");
-
-  // function increaseObject() {
-  //   setObj((prev) => {
-  //     prev.value = prev.value + 1; // 내부 값만 변경
-  //     return prev; // 같은 객체 반환
-  //   });
-  // }
 
   function increaseObject() {
-    objRef.current.value += 1; // ✅ 참조값 유지하면서 내부 값만 변경
+    objRef.current.value += 1;
     console.log("Updated objRef:", objRef.current);
   }
 
-  // console.log(a(), b());
+  function handleHover() {
+    console.log(" 버튼 위에 마우스 올림", buttonRef.current);
+  }
 
   document.getElementById("app").innerHTML = `
-    <h1>Count: ${getCount()}</h1>
+    <h1>Count: ${objRef.current.value}</h1>
     <button id="a"> 의존성에 넣을 예정 ${a()}</button>
     <button id="b">의존성에 넣지 않을 예정 ${b()}</button>
     <button id="c">참조값</button>
-
+    <button id="myButton">포커스 버튼</button>
   `;
 
   document.getElementById("a").addEventListener("click", handleUpdateText);
   document.getElementById("b").addEventListener("click", increase);
   document.getElementById("c").addEventListener("click", increaseObject);
+
+  buttonRef.current = document.getElementById("myButton");
+  buttonRef.current.onmouseover = handleHover;
 }
 
 App();
