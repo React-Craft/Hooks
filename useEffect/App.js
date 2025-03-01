07f54730f269aab944c5d35990e5fead4233b075
YOON-CC <whdbscks77@gmail.com>
2025-03-01 16:00:17 +0900

    style: useEffect 부분 주석 제거

--- useEffect/App.js
+++ useEffect/App.js
@@ -5,16 +5,17 @@ function App() {
   const [getCount, setCount] = useState(0);
   const [a, setA] = useState("True");
   const [b, setB] = useState(0);
-
+  const [obj, setObj] = useState({ value: 0 });
   useEffect(() => {
     console.log("🎯🎯🎯🎯🎯🎯🎯🎯🎯🎯🎯🎯🎯🎯");
-    setCount(getCount() + 1);
-  }, [a()]);
+    // setCount(getCount() + 1);
+  }, [obj().value]);
 
   // useEffect(() => {
   //   console.log("🙌🙌🙌🙌🙌🙌🙌🙌🙌🙌🙌🙌🙌🙌");
   //   setCount(getCount() + 1);
   // }, [a(), b()]);
+  console.log(obj().value);
 
   function handleUpdateText() {
     setA(a() === "True" ? "false" : "True");
@@ -24,16 +25,23 @@ function App() {
     setB(b() + 1);
   }
 
+  function increaseObject() {
+    setObj({ value: obj().value });
+  }
+
   // console.log(a(), b());
 
   document.getElementById("app").innerHTML = `
     <h1>Count: ${getCount()}</h1>
     <button id="a"> 의존성에 넣을 예정 ${a()}</button>
     <button id="b">의존성에 넣지 않을 예정 ${b()}</button>
+    <button id="c">참조값</button>
+
   `;
 
   document.getElementById("a").addEventListener("click", handleUpdateText);
   document.getElementById("b").addEventListener("click", increase);
+  document.getElementById("c").addEventListener("click", increaseObject);
 }
 
 App();
--- useEffect/useEffect.js
+++ useEffect/useEffect.js
@@ -4,7 +4,6 @@ let firstRender = false;
 function useEffect(callback, dependencies) {
   const prevDependencies = useEffectStore[stateStore.stateIndex] || [];
 
-  // 의존성 없으면 바로 실행
   if (dependencies === undefined) {
     setTimeout(callback, 0);
     return;
@@ -21,17 +20,11 @@ function useEffect(callback, dependencies) {
   if (dependencies) {
     for (let i = 0; i < dependencies.length; i++) {
       if (dependencies[i] !== prevDependencies[i]) {
-        // console.log("없음, 지금", i);
         isChanged = true;
         break;
       }
     }
   }
-
-  // console.log(useEffectStore);
-  // console.log(useEffectStore[stateIndex]);
-  // console.log(dependencies);
-  // console.log("바뀌었는지", isChanged);
 
   if (isChanged) {
     setTimeout(callback, 0);
