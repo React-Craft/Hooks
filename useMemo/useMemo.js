6656e378ed95778dbb47b3081c57028a7aa65d9d
YOON-CC <whdbscks77@gmail.com>
2025-03-14 00:40:14 +0900

    feat: useMemoStore 초기화

--- useMemo/useMemo.js
+++ useMemo/useMemo.js
@@ -4,5 +4,11 @@ function useMemo(callback, dependencies) {
   const currentIndex = stateStore.stateIndex;
   stateStore.stateIndex++;
 
+  if (!useMemoStore[currentIndex]) {
+    useMemoStore[currentIndex] = { dependencies: undefined, value: undefined };
+  }
+
+  console.log(useMemoStore[currentIndex], currentIndex);
+
   return currentIndex;
 }
